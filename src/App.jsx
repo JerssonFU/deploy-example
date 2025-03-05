39af187a57788f663fa68f7938acdb243918b1f7
JerssonFU <jersson.zynths@gmail.com>
2025-03-05 18:30:16 -0500

    five Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import PageWithNavbar from "./pages/PageWithNavbar";
 import Home from "./pages/Home";
 import ContactPage from "./pages/ContactPage";
-import ReactPage from "./pages/CertificacionesPage";  // Importa la nueva página
-import JavaPage from "./pages/PowerBiPage";    // Importa la nueva página
-import SMSSPage from "./pages/SMSSPage";    // Importa la nueva página
-import BIAPage from "./pages/PythonPage";      // Importa la nueva página
+import ReactPage from "./pages/CertificacionesPage";
+import JavaPage from "./pages/PowerBiPage";
+import SMSSPage from "./pages/SMSSPage";
+import BIAPage from "./pages/PythonPage";
 import PowerBIPage from "./pages/PowerBiPage";
 
 function App() {
   return (
-    <Router basename="deploy-example">
+    <Router>
       <Routes>
-        {/* Página de inicio */}
         <Route path="/" element={<Home />} />
-
-        {/* Página con Navbar */}
         <Route path="/Inicio" element={<PageWithNavbar />} />
-
-        {/* Página de contacto */}
         <Route path="/contact" element={<ContactPage />} />
-
-        {/* Nuevas páginas */}
         <Route path="/Certificaciones" element={<ReactPage />} />
         <Route path="/PowerBI" element={<PowerBIPage />} />
         <Route path="/SqlServer" element={<SMSSPage />} />
         <Route path="/Python" element={<BIAPage />} />
+        {/* Redirección en caso de rutas desconocidas */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
