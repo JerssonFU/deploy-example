b99804f33018cc49b6e268f36f400415de6e8746
JerssonFU <jersson.zynths@gmail.com>
2025-03-06 13:12:27 -0500

    Solucion final

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import PageWithNavbar from "./pages/PageWithNavbar";
 import Home from "./pages/Home";
 import ContactPage from "./pages/ContactPage";
-import ReactPage from "./pages/CertificacionesPage";
-import JavaPage from "./pages/PowerBiPage";
-import SMSSPage from "./pages/SMSSPage";
-import BIAPage from "./pages/PythonPage";
+import ReactPage from "./pages/CertificacionesPage";  // Importa la nueva página
+import JavaPage from "./pages/PowerBiPage";    // Importa la nueva página
+import SMSSPage from "./pages/SMSSPage";    // Importa la nueva página
+import BIAPage from "./pages/PythonPage";      // Importa la nueva página
 import PowerBIPage from "./pages/PowerBiPage";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
+        {/* Página de inicio */}
         <Route path="/" element={<Home />} />
+
+        {/* Página con Navbar */}
         <Route path="/Inicio" element={<PageWithNavbar />} />
+
+        {/* Página de contacto */}
         <Route path="/contact" element={<ContactPage />} />
+
+        {/* Nuevas páginas */}
         <Route path="/Certificaciones" element={<ReactPage />} />
         <Route path="/PowerBI" element={<PowerBIPage />} />
         <Route path="/SqlServer" element={<SMSSPage />} />
         <Route path="/Python" element={<BIAPage />} />
-        {/* Redirección en caso de rutas desconocidas */}
-        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/deploy-example/", // Asegúrate de que coincide con tu repo en GitHub Pages
+  base:"/deploy-example"
 });
