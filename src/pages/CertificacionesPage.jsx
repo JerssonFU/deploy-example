83f478cf21312f6771badf64c644e98c747ad8f6
JerssonFU <jersson.zynths@gmail.com>
2025-03-10 13:10:14 -0500

    V14

--- src/pages/CertificacionesPage.jsx
+++ src/pages/CertificacionesPage.jsx
@@ -7,14 +7,16 @@ import cert2 from "/assets/cert2.png";
 import cert3 from "/assets/cert3.png";
 import cert4 from "/assets/cert4.png";
 
+const baseURL = "/deploy-example"; // Cambia esto según el repositorio
 const certificaciones = [
-  { image: cert1, link: "/pdf/Cert1.pdf" },
-  { image: cert2, link: "/pdf/Cert2.pdf" },
-  { image: cert3, link: "/pdf/Cert3.pdf" },
-  { image: cert4, link: "/pdf/Cert4.pdf" },
-  { image: cert1, link: "/pdf/Cert1.pdf" },
-  { image: cert1, link: "/pdf/Cert1.pdf" }
+  { image: cert1, link: `${baseURL}/pdf/Cert1.pdf` },
+  { image: cert2, link: `${baseURL}/pdf/Cert2.pdf` },
+  { image: cert3, link: `${baseURL}/pdf/Cert3.pdf` },
+  { image: cert4, link: `${baseURL}/pdf/Cert4.pdf` },
+  { image: cert1, link: `${baseURL}/pdf/Cert1.pdf` },
+  { image: cert1, link: `${baseURL}/pdf/Cert1.pdf` }
 ];
+
 
 function CertificacionesPage() {
   const [modalOpen, setModalOpen] = useState(false);
@@ -51,14 +53,15 @@ function CertificacionesPage() {
       </div>
 
       {/* 📌 Modal para mostrar el certificado en PDF */}
-      {modalOpen && selectedCert && (
-        <div className="modal">
-          <div className="modal-content">
-            <span className="close-btn" onClick={closeModal}>&times;</span>
-            <iframe src={selectedCert.link} className="pdf-viewer"></iframe>
-          </div>
-        </div>
-      )}
+{modalOpen && selectedCert && (
+  <div className="modal">
+    <div className="modal-content">
+      <span className="close-btn" onClick={closeModal}>&times;</span>
+      <iframe src={`${selectedCert.link}#page=1`} className="pdf-viewer"></iframe>
+    </div>
+  </div>
+)}
+
     </div>
   );
 }
