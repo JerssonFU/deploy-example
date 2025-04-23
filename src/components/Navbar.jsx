ca622f2edf85a97cb233d444998450c96c23240d
JerssonFU <jersson.zynths@gmail.com>
2025-04-23 16:42:00 -0500

    Todo Marcha Bien3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,10 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>Mi portafolio</NavLink>
+        <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>Lobby</NavLink>
         <NavLink to="/Inicio" className={({ isActive }) => isActive ? "active" : ""}>Inicio</NavLink>
         <NavLink to="/Certificaciones" className={({ isActive }) => isActive ? "active" : ""}>Certificaciones</NavLink>
-        <NavLink to="/PowerBI" className={({ isActive }) => isActive ? "active" : ""}>PowerBI</NavLink>
+        <NavLink to="/PowerBI" className={({ isActive }) => isActive ? "active" : ""}>Power BI</NavLink>
         <NavLink to="/SMSS" className={({ isActive }) => isActive ? "active" : ""}>SMSS</NavLink>
         <NavLink to="/Python" className={({ isActive }) => isActive ? "active" : ""}>Python</NavLink>
       </div>
--- src/pages/PageWithNavbar.jsx
+++ src/pages/PageWithNavbar.jsx
@@ -6,8 +6,7 @@ function PageWithNavbar() {
     <div className="main-content">
       <div className="hero-container">
         <div className="hero-text">
-          <h1 className="hero-title">Perfil Profesional</h1>
-          <h3 className="hero-subtitle">Business Intelligence Analyst</h3>
+          <h1 className="hero-title">Perfil Profesional </h1>
           <h2 className="description">
             Me especializo en el análisis, transformación y visualización de datos utilizando herramientas BI para optimizar la toma de decisiones en las organizaciones. Además, cuento con conocimientos en Front-End, lo que me permite estructurar y presentar la información de manera clara y eficiente, mejorando la experiencia del usuario.
           </h2>
@@ -19,27 +18,21 @@ function PageWithNavbar() {
         <div className="tools-grid">
           <div className="tool-box">
             <img src="./assets/sql.png" alt="SQL Server" className="tool-img" />
-            <p className="tool-info">SQL Server</p>
           </div>
           <div className="tool-box">
             <img src="./assets/react.png" alt="React" className="tool-img" />
-            <p className="tool-info">React</p>
           </div>
           <div className="tool-box">
             <img src="./assets/excel.png" alt="Excel" className="tool-img" />
-            <p className="tool-info">Excel</p>
           </div>
           <div className="tool-box">
             <img src="./assets/powerbi.png" alt="Power BI" className="tool-img" />
-            <p className="tool-info">Power BI</p>
           </div>
           <div className="tool-box">
             <img src="./assets/git.png" alt="Git" className="tool-img" />
-            <p className="tool-info">Git</p>
           </div>
           <div className="tool-box">
             <img src="./assets/python.png" alt="Python" className="tool-img" />
-            <p className="tool-info">Python</p>
           </div>
         </div>
       </div>
